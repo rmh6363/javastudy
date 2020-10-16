bc990bdb68f5367f0e60a75acb19694b5e1627ad
rmh <rmh63@naver.com>
2020-10-16 17:44:56 +0900

    강의 프로젝트

---
+++ TypeEx/src/kr/co/ezen/MainEx.java
@@ -0,0 +1,69 @@
+package kr.co.ezen;
+
+public class MainEx {
+	public static void main(String[] args) {		
+		boolean alpha = true;
+		boolean beta = false;
+		char ceta = 'F';
+		byte ff = 127;
+		int gg = 123123213;
+		long hh = 123123123123123L;
+		float asd = 3.141414F;
+		double zxc = 3.1415928432;
+		boolean ms = true;
+		boolean bs = false;
+		char pp = 'A';
+		int good = 2000000000;
+		System.out.println(ff);
+		System.out.println(gg);
+		System.out.println(hh);
+		System.out.println(asd);
+		System.out.println(zxc);
+		System.out.println(ceta);
+		System.out.println(alpha);
+		System.out.println((beta));
+		System.out.println(ms);
+		System.out.println(bs); 
+		System.out.println(pp);
+		System.out.println(good);
+		System.out.println(asd+zxc);
+		System.out.println(pp+ceta);
+		System.out.println(asd+1);
+		System.out.println(ff+gg+asd+zxc);
+		System.out.println(ff*3);
+		System.out.println(ff/2);
+		System.out.println(1);
+		System.out.println(1+2+3+4+5+6+7+8+9+10);
+		System.out.println(asd+500);
+		float kim = 3.1523123F;
+		System.out.println(kim);
+		double ro = 3.123123123;
+		System.out.println(ro+20);
+		boolean min = true;
+		boolean il = false;
+		System.out.println(min);
+		System.out.println(il);
+		char ask = 'гн';
+		System.out.println(ask);
+		String aAa = "ddd";
+		String pipa = new String("ptsd");
+		System.out.println(aAa+pipa);
+		System.out.println(aAa+ro);
+		System.out.println(alpha);
+		int pay = 2500000;
+		System.out.println(pay);
+		int bonus = 300000;
+		System.out.println(pay+bonus);
+		System.out.println(30000000*0.095);
+		boolean answer = false;
+		boolean answer2 = true;
+		System.out.println(answer);
+		System.out.println(answer2);
+		char result = 'F';
+		char result2 = 'A';
+		System.out.println(result);
+		System.out.println(result2);
+		
+		
+	}
+}
