cd6dc97f5a0da73face3d9c4398f409a35a93301
rmh <rmh63@naver.com>
2020-11-03 17:30:24 +0900

    20201103

---
+++ ArrayEx/src/com/naver/Dog.java
@@ -0,0 +1,20 @@
+package com.naver;
+
+public class Dog {
+	private String name;
+	public Dog() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Dog(String name) {
+		super();
+		this.name = name;
+	}
+	
+
+}
---
+++ ArrayEx/src/kr/co/ezen/MainEx.java
@@ -0,0 +1,42 @@
+package kr.co.ezen;
+
+import com.naver.Dog;
+import com.naver.Test;
+
+public class MainEx {
+	//n차원 배열 안에서는 n-1 차원의 배열이 들어 있다
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test t1 = new Test();
+		
+		t1.me11();
+		t1.me12(new Dog[5]);
+		Dog[] arrDog = new Dog[3];
+		t1.me12(arrDog);
+		
+		
+		t1.me2();
+		t1.me21();
+		String[] arrStr = {"hello","world","good"};
+		t1.me22(arrStr);
+		t1.me31();
+		Dog[] dogs = new Dog[]{new Dog(), new Dog(),new Dog()};
+		t1.me32(dogs);
+		t1.me33();
+		t1.me3();
+		System.out.println("#####");
+		t1.me01();
+		t1.me1();
+		t1.me13();
+		t1.me1();
+		t1.name();
+		t1.gugudan();
+		t1.name2();
+		
+			
+		
+		
+	}
+
+}
