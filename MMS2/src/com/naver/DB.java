49afb971738e80e0abc0887bafd82d809eec3ee6
rmh <rmh63@naver.com>
2020-11-11 09:28:55 +0900

    20201111Test

--- MMS2/src/com/naver/DB.java
+++ MMS2/src/com/naver/DB.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class DB {
 	public static List<MemberDTO> db = new ArrayList<MemberDTO>();
+	
 
 	
 
--- MMS2/src/com/naver/SelectCommand.java
+++ MMS2/src/com/naver/SelectCommand.java
@@ -9,9 +9,13 @@ public class SelectCommand implements Command{
 		for (int i = 0; i < DB.db.size(); i++) {
 			MemberDTO dto = DB.db.get(i);
 			System.out.println(dto);
+			
+			
 		}
-		
+
 	}
+
+	
 
 	
 	}
